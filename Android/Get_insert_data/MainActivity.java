f04a58b332134600afb697f881f34c7713cff6a9
Kornelius Pasztor <korn_20000@hotmail.com>
2017-10-28 22:49:04 +0200

    Updated PhP and added insert codes
    ID 4+18

--- Android/Get_insert_data/MainActivity.java
+++ Android/Get_insert_data/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,11 +22,14 @@ public class MainActivity extends AppCompatActivity
     String JSON_STRING;
     String json_string;
 
+    History hist = new History();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hist.getClass();
     }
 
     public void getJSON(View view)
@@ -102,5 +106,11 @@ public class MainActivity extends AppCompatActivity
             intent.putExtra("json_data", json_string);
             startActivity(intent);
         }
+    }
+
+    public void sendData(View view)
+    {
+        Intent intent = new Intent(this, History.class);
+        startActivity(intent);
     }
 }
